b50d5708f223f7e9202c1d72264894f14a7bee6d
오진서 <s22030@gsm.hs.kr>
2024-02-19 15:43:30 +0900

    initial commit

---
+++ src/main/java/JTpayment/MartInTown/MartInTownApplication.java
@@ -0,0 +1,13 @@
+package JTpayment.MartInTown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MartInTownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MartInTownApplication.class, args);
+	}
+
+}
---
+++ src/test/java/JTpayment/MartInTown/MartInTownApplicationTests.java
@@ -0,0 +1,13 @@
+package JTpayment.MartInTown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MartInTownApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
