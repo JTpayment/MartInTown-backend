efcfa45bc8405d1b50bc8fedcd71ec48a039a3ed
오진서 <s22030@gsm.hs.kr>
2024-02-20 21:24:09 +0900

    create :: custom handler

---
+++ src/main/java/JTpayment/MartInTown/global/security/handler/CustomAccessDeniedHandler.java
@@ -0,0 +1,25 @@
+package JTpayment.MartInTown.global.security.handler;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@Slf4j
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+    @Override
+    public void handle(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            AccessDeniedException accessDeniedException
+    ) throws ServletException, IOException {
+        log.error("ACCESS DENIED");
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+    }
+}
