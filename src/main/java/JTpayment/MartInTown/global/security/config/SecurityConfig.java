27aa572017c00b5df7c12567c6837394b21f1826
오진서 <s22030@gsm.hs.kr>
2024-02-20 21:23:08 +0900

    create :: security config

---
+++ src/main/java/JTpayment/MartInTown/global/security/config/SecurityConfig.java
@@ -0,0 +1,68 @@
+package JTpayment.MartInTown.global.security.config;
+
+import JTpayment.MartInTown.global.filter.JwtRequestFilter;
+import JTpayment.MartInTown.global.security.handler.CustomAccessDeniedHandler;
+import JTpayment.MartInTown.global.security.handler.CustomAuthenticationEntryPointHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.CorsUtils;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig {
+    private final JwtRequestFilter jwtRequestFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity
+                .cors().configurationSource(corsConfigurationSource()).and()
+                .csrf().disable();
+        httpSecurity
+                .exceptionHandling()
+                .accessDeniedHandler(new CustomAccessDeniedHandler())
+                .authenticationEntryPoint(new CustomAuthenticationEntryPointHandler());
+        httpSecurity
+                .httpBasic().disable() //UI, UX Disable
+                .formLogin().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeHttpRequests()
+                .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
+                .requestMatchers(HttpMethod.POST, "/auth/signup").permitAll()
+                .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                .requestMatchers(HttpMethod.PATCH, "/auth/reissue").permitAll()
+                .requestMatchers(HttpMethod.DELETE, "/auth/logout").permitAll()
+                .requestMatchers(HttpMethod.POST, "/mail").permitAll()
+                .requestMatchers(HttpMethod.POST, "/mail/check").permitAll()
+                .anyRequest().permitAll();
+
+        httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.addAllowedOriginPattern("*");
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
