56bf67b0444184b0a0a074ac1dfd6568b5c83c1a
오진서 <s22030@gsm.hs.kr>
2024-02-20 21:39:52 +0900

    add :: some annotations

--- src/main/java/JTpayment/MartInTown/MartInTownApplication.java
+++ src/main/java/JTpayment/MartInTown/MartInTownApplication.java
@@ -2,8 +2,21 @@ package JTpayment.MartInTown;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableConfigurationProperties
+@ConfigurationPropertiesScan
+@EnableCaching
+@EnableScheduling
+@EnableJpaRepositories
+@EnableWebMvc
+
 public class MartInTownApplication {
 
 	public static void main(String[] args) {
