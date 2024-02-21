2f32eb0030d5af7d50ebebcd6f7903930e383363
hothuuk <s22014@gsm.hs.kr>
2024-02-21 19:09:28 +0900

    :memo: Register 관련 에러 추가

--- src/main/java/JTpayment/MartInTown/global/error/ErrorCode.java
+++ src/main/java/JTpayment/MartInTown/global/error/ErrorCode.java
@@ -20,7 +20,10 @@ public enum ErrorCode {
     //S3
     FILE_UPLOAD_FAIL("파일 업로드에 실패했습니다.", 500),
     NOT_ALLOWED_FILE("허용되지 않은 파일 형식입니다.", 400),
-    INVALID_FORMAT_FILE("잘못된 형식의 파일입니다.", 400);
+    INVALID_FORMAT_FILE("잘못된 형식의 파일입니다.", 400),
+
+    //REGISTER
+    REGISTER_NOT_FOUND("점포 신청을 찾을 수 없습니다.", 404);
 
     private final String message;
     private final int status;
