b6864f60e590ae39fa976eb5bb348dee18c6d081
hothuuk <s22014@gsm.hs.kr>
2024-02-21 17:11:34 +0900

    :memo: Register 작성

---
+++ src/main/java/JTpayment/MartInTown/domain/register/entity/Register.java
@@ -0,0 +1,32 @@
+package JTpayment.MartInTown.domain.register.entity;
+
+import JTpayment.MartInTown.domain.register.entity.enums.Status;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Register {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long registerId;
+
+    @Column(nullable = false)
+    private Long memberId;
+
+    @Column(nullable = false)
+    private String storeName;
+
+    @Column(nullable = false)
+    private String address;
+
+    private String image;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Status status;
+}
