e45b63c01cfeb850ace8838044dd00047220ff66
hothuuk <s22014@gsm.hs.kr>
2024-02-23 16:02:03 +0900

    :memo: StoreListService 작성

---
+++ src/main/java/JTpayment/MartInTown/domain/store/service/StoreListService.java
@@ -0,0 +1,8 @@
+package JTpayment.MartInTown.domain.store.service;
+
+import JTpayment.MartInTown.domain.store.presentation.dto.response.StoreListResponse;
+
+public interface StoreListService {
+
+    StoreListResponse execute();
+}
