6b472c358b14826ac104579d39727f5b876f9606
hothuuk <s22014@gsm.hs.kr>
2024-02-28 18:10:05 +0900

    :memo: MANAGER_MISMATCH 추가

--- src/main/java/JTpayment/MartInTown/global/error/ErrorCode.java
+++ src/main/java/JTpayment/MartInTown/global/error/ErrorCode.java
@@ -24,9 +24,12 @@ public enum ErrorCode {
 
     //REGISTER
     REGISTER_NOT_FOUND("점포 신청을 찾을 수 없습니다.", 404),
-    STORE_NOT_FOUND("점포를 찾을 수 없습니다.", 404),
     DUPLICATED_FAVORITE_STORE("중복된 단골점포 입니다.", 409),
-    FAVORITE_STORE_NOT_FOUND("단골 점포를 찾을 수 없습니다.", 404);
+    FAVORITE_STORE_NOT_FOUND("단골 점포를 찾을 수 없습니다.", 404),
+
+    //STORE
+    STORE_NOT_FOUND("점포를 찾을 수 없습니다.", 404),
+    MANAGER_MISMATCH("점포 주인이 아닙니다.", 403);
 
     private final String message;
     private final int status;
